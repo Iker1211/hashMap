4beb645df2f09a0038681a198c0ee7d4ac30e6bf
iker <velezlucasiker1@gmail.com>
2025-08-21 17:57:31 -0500

    Add eslint and prettier

--- hashMap.js
+++ hashMap.js
@@ -1,166 +1,169 @@
 class Node {
-    constructor(key, value = null, next = null) {
-        this.key = key;
-        this.value = value;
-        this.next = next;
-    }
+  constructor(key, value = null, next = null) {
+    this.key = key;
+    this.value = value;
+    this.next = next;
+  }
 }
 
 export default class HashMap {
-    constructor() {
-        this.bucketsArray = new Array(16).fill(null);
-        this.loadFactor = 0.75;
-        this.capacity = this.bucketsArray.length;
-        this.occupied = 0;
-    }
+  constructor() {
+    this.bucketsArray = new Array(16).fill(null);
+    this.loadFactor = 0.75;
+    this.capacity = this.bucketsArray.length;
+    this.occupied = 0;
+  }
 
-    hash(key) {
-        let hashKey = 0;
-        const prime = 31;
-        for (let i = 0; i < key.lenght; i++) {
-            hashKey +=  key.charCodeAt(i) * prime;  
-        }
-        return hashKey % this.bucketsArray.length;
+  hash(key) {
+    let hashKey = 0;
+    const prime = 11;
+    for (let i = 0; i < key.lenght; i += 1) {
+      hashKey += key.charCodeAt(i) * prime;
     }
+    return hashKey % this.bucketsArray.length;
+  }
 
-    set(key, value) {
-        if (this.occupied / this.capacity >= this.loadFactor ) {
-            this.resize();
-        }
-        const bucket = this.hash(key);
-        if (!this.hash(key)) {
-            const newNode = new Node(key, value);
-            if (this.bucketsArray[bucket] === null) {
-                this.occupied += 1;
-                this.bucketsArray[bucket] = newNode;
-            } else {
-                let current = this.bucketsArray[bucket];
-                while (current.next !== null) {
-                    current.current.next;
-                }
-                current.next = newNode;
-            }
-        } else {
-            let current = this.bucketsArray[bucket];
-            while (current !== null && current.key !== key) {
-                current.current.next;
-            }
-            if (current !== null) {
-                current.value = value;
-            }
-        }
+  set(key, value) {
+    if (this.occupied / this.capacity >= this.loadFactor) {
+      this.resize();
     }
-
-    get(key) {
-       const bucket = this.hash(key);
-       let current = this.bucketsArray[bucket];
-       while (current.key !== null && current.key !== key) {
-        current = current.next;
-       }
-       if (current == null) {
-        return null;
-       }
-       return current.key;
-    }
-
-    has(key) {
-        const bucket = this.hash(key);
+    const bucket = this.hash(key);
+    if (!this.hash(key)) {
+      const newNode = new Node(key, value);
+      if (this.bucketsArray[bucket] === null) {
+        this.occupied += 1;
+        this.bucketsArray[bucket] = newNode;
+      } else {
         let current = this.bucketsArray[bucket];
-        while (current !== null) {
-            if (current.key === key) {
-                return true;
-            }
-            current = current.next;
+        while (current.next !== null) {
+          current = current.next;
         }
-        return false;
+        current.next = newNode;
+      }
+    } else {
+      let current = this.bucketsArray[bucket];
+      while (current !== null && current.key !== key) {
+        current.current.next;
+      }
+      if (current !== null) {
+        current.value = value;
+      }
+    }
+  }
+
+  get(key) {
+    const bucket = this.hash(key);
+    let current = this.bucketsArray[bucket];
+    while (current.key !== null && current.key !== key) {
+      current = current.next;
+    }
+    if (current == null) {
+      return null;
+    }
+    return current.key;
+  }
+
+  has(key) {
+    const bucket = this.hash(key);
+    let current = this.bucketsArray[bucket];
+    while (current !== null) {
+      if (current.key === key) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  remove(key) {
+    const bucket = this.hash(key);
+    let current = this.bucketsArray[bucket];
+    let previous = null;
+
+    while (current !== null && current.key !== key) {
+      previous = current;
+      current = current.next;
     }
 
-    remove(key) {
-       const bucket = this.hash(key);
-       let current = this.bucketsArray[bucket];
-       let previous = null; 
-
-       while (current !== null && current.key !== key) {
-        previous = current;
-        current = current.next;
-       }
-
-       if (current = null) {
-        return;
-       }
-
-       if (previous === null && current.next === null) {
-        this.occupied -= 1;
-        this.bucketsArray[bucket] = current.next;
-       } else if (previous === null) {
-        this.bucketsArray[bucket] = current.next;
-       } else {
-        previous.next = current.next;
-       }
+    if (current === null) {
+      return; // Key not found
     }
 
-    length() {
-        let counter = 0;
-        this.bucketsArray.forEach((bucket) => {
-            let current = bucket;
-            if (bucket !== null) {
-                counter += 1;
-                while (current.next !== null) {
-                    counter +=1;
-                    current = current.next;
-                }
-            }
-        });
-        return counter;
+    if (previous === null && current.next === null) {
+      // Remove first node that has no successors
+      this.occupied -= 1;
+      this.bucketsArray[bucket] = current.next;
+    } else if (previous === null) {
+      // Remove first node that has successors
+      this.bucketsArray[bucket] = current.next;
+    } else {
+      // Remove node and connects the next
+      previous.next = current.next;
     }
+  }
 
-    clear() {
-        this.bucketsArray = new Array(16).fill(null);
-        this.occupied = 0;
-    }
+  length() {
+    let counter = 0;
+    this.bucketsArray.forEach((bucket) => {
+      let current = bucket;
+      if (bucket !== null) {
+        counter += 1;
+        while (current.next !== null) {
+          counter += 1;
+          current = current.next;
+        }
+      }
+    });
+    return counter;
+  }
 
-    keys() {
-        const arrayOfKeys = [];
-        this.bucketsArray.forEach((bucket) => {
-            let current = bucket;
-            if (bucket !== null) {
-                arrayOfKeys.push(current.key);
-                while (current.next !== null) {
-                    current = current.next;
-                    arrayOfKeys.push(current.key)
-                }
-            }
-        });
-        return arrayOfKeys;
-    }
+  clear() {
+    this.bucketsArray = new Array(16).fill(null);
+    this.occupied = 0;
+  }
 
-    values() {
-        const arrayOfValues = [];
-        this.bucketsArray.forEach((bucket) => {
-            let current = bucket;
-            if (bucket !== null) {
-                arrayOfValues.push(current.value);
-                while (current.next !== null) {
-                    current = current.next;
-                    arrayOfValues.push(current.value);
-                }
-            }
-        });
-        return arrayOfValues;
-    }
+  keys() {
+    const arrayOfKeys = [];
+    this.bucketsArray.forEach((bucket) => {
+      let current = bucket;
+      if (bucket !== null) {
+        arrayOfKeys.push(current.key);
+        while (current.next !== null) {
+          current = current.next;
+          arrayOfKeys.push(current.key);
+        }
+      }
+    });
+    return arrayOfKeys;
+  }
 
-    entries() {
-        const arrayOfEntries = [];
-        this.bucketsArray.forEach((bucket) => {
-            let current = bucket;
-            if (bucket !== null) {
-                arrayOfEntries.push([current.key, current.value]);
-                while (current.next !== null) {
-                    current = current.next;
-                    arrayOfEntries.push([current.key, current.value]);
-                }
-            }
-        });
-        return arrayOfEntries;
-    }
+  values() {
+    const arrayOfValues = [];
+    this.bucketsArray.forEach((bucket) => {
+      let current = bucket;
+      if (bucket !== null) {
+        arrayOfValues.push(current.value);
+        while (current.next !== null) {
+          current = current.next;
+          arrayOfValues.push(current.value);
+        }
+      }
+    });
+    return arrayOfValues;
+  }
+
+  entries() {
+    const arrayOfEntries = [];
+    this.bucketsArray.forEach((bucket) => {
+      let current = bucket;
+      if (bucket !== null) {
+        arrayOfEntries.push([current.key, current.value]);
+        while (current.next !== null) {
+          current = current.next;
+          arrayOfEntries.push([current.key, current.value]);
+        }
+      }
+    });
+    return arrayOfEntries;
+  }
 }
